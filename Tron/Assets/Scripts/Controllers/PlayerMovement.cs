e4cf83c3d02ad8acadf0190305dfc0fcde47aefa
Robin Leman <robin.leman@mail.mcgill.ca>
2020-02-19 13:44:51 -0500

    Organization updated

--- Tron/Assets/Scripts/Controllers/PlayerMovement.cs
+++ Tron/Assets/Scripts/Controllers/PlayerMovement.cs
@@ -1,22 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     private Vector2 dir;
-    private Vector2 pos;
 
-    [SerializeField] private GameObject trackPrefab;
-    [SerializeField] private int playerID;
     [SerializeField] private Vector2 startPos = Vector2.up;
-    [SerializeField] private Transform trackFolder;
+    [SerializeField] private int playerID;
 
-    List<Transform> track = new List<Transform>();
-    
- 
     void Start()
     {
         dir = startPos;
@@ -37,24 +30,6 @@ public class PlayerMovement : MonoBehaviour
     void Move()
     {
         transform.Translate(dir);
-
-        pos = transform.position;
-
-        if (track.Count > 0) {
-            // Move last Tail Element to where the Head was, it will superpose with the player so ignore the collision
-            track.Last().gameObject.tag = "Untagged";
-            track.Last().position = pos;
-            // Add to front of list, remove from the back
-            track.Insert(0, track.Last());
-            track.RemoveAt(track.Count-1);
-        }
-        if (track.Count >= 2){
-            // Start to detect collision after 2 tracks
-            track[1].gameObject.tag = "Track";
-        }
-
-        IncreaseTail();
-
     }
 
     void inputControlsPlayer1(){
@@ -79,13 +54,5 @@ public class PlayerMovement : MonoBehaviour
             dir = -Vector2.right;
         else if (Input.GetKey(KeyCode.W) && dir != (-Vector2.up))
             dir = Vector2.up;
-    }
-
-    void IncreaseTail(){
-        // Load Prefab into the world
-        GameObject trackPart = (GameObject)Instantiate(trackPrefab, pos, Quaternion.identity);
-        trackPart.transform.SetParent(trackFolder);
-        // Keep track of it in our tail list
-        track.Insert(0, trackPart.transform);
     }
 }
---
+++ Tron/Assets/Scripts/Managers/TrackManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TrackManager : MonoBehaviour
+{
+
+    private Vector2 pos;
+
+    [SerializeField] private GameObject trackPrefab;
+    [SerializeField] private Transform trackFolder;
+
+    List<Transform> track = new List<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTail", 0.2f, 0.2f);
+    }
+
+    void UpdateTail(){
+        
+        pos = transform.position;
+
+        if (track.Count > 0) {
+            // Move last Tail Element to where the Head was, it will superpose with the player so ignore the collision
+            track.Last().gameObject.tag = "Untagged";
+            track.Last().position = pos;
+            // Add to front of list, remove from the back
+            track.Insert(0, track.Last());
+            track.RemoveAt(track.Count-1);
+        }
+        if (track.Count >= 2){
+            // Start to detect collision after 2 tracks
+            track[1].gameObject.tag = "Track";
+        }
+
+        IncreaseTail();
+
+    }
+
+    void IncreaseTail(){
+        // Load Prefab into the world
+        GameObject trackPart = (GameObject)Instantiate(trackPrefab, pos, Quaternion.identity);
+        trackPart.transform.SetParent(trackFolder);
+        // Keep track of it in our tail list
+        track.Insert(0, trackPart.transform);
+    }
+}
